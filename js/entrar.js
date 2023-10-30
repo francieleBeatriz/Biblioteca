36cc311b8a1a7360cb10034234245e105d738e40
Pablio Richardy <yr0p@edu.uniube.br>
2023-10-30 10:42:43 -0300

    Adicionando as modificações de MAIN e alguns ajustes

---
+++ js/entrar.js
@@ -0,0 +1,51 @@
+import { mensagem } from "./mensagensFeedback.js"; 
+
+let btnEntrar = document.querySelector("#sing-in");
+
+btnEntrar.addEventListener("click", () => entrar())
+
+function entrar()
+{
+    let msg = document.querySelector("#mensagens");
+
+    let user = document.querySelector("#usuario");
+    let password = document.querySelector("#senha");
+
+    fetch(
+        "http://localhost/api.biblioteca/auth",
+        {
+            method: "POST",
+            body: JSON.stringify({
+                "user": user.value,
+                "password": password.value,
+            })
+        }
+    ).then(
+        function (response)
+        {
+            return response.json();
+        }
+    ).then(
+        function (response)
+        {
+            msg.style.display = "block";
+            if(response.status == 200)
+            {
+                localStorage.setItem("token", response.token);
+                window.location.href = "index.html";
+            }
+            msg.innerHTML = mensagem(response.status, response.message);
+
+        }
+    ).catch(
+        function (response)
+        {
+            msg.style.display = "block";
+            msg.innerHTML = mensagem(response.stauts, "Ocorreu um erro inesperado! Por favor, tente novamente mais tarde!");
+        }
+    )
+
+    user.value = "";
+    password.value = "";
+}
+
